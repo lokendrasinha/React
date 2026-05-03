a9733bb2d56632af899c88f0a0d787684b793c33
lokendrasinha <lokendrasinha2003@gmail.com>
2026-05-03 23:27:16 +0530

    Folder structure created using vite

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="dad">
+      <h1>Hello, World!</h1>
+    </div>
+    <div id='chacha'>
+      <h1>Welcome to React</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
