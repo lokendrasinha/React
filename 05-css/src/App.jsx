83498fcf5db29250cdf28315e4a522488e49aa6a
lokendrasinha <lokendrasinha2003@gmail.com>
2026-05-05 18:11:05 +0530

    Added Modular css for header and button

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.css'
+import Button from './components/button/Button.jsx'
+import Header from './components/header/Header.jsx'
+const App = () => {
+  return (
+    <div>
+      <Button/>
+      <Header/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h3 className={styles.title}>Rajat</h3>
+        <button className={styles.btn}>Login</button>
+    </div>
+  )
+}
+
+export default Header
